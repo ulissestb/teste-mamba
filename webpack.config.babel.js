edc4a354b4ffec60893e7d4753e617827a57eefc
Christian Kaisermann <christian@kaisermann.me>
2018-03-21 14:01:09 -0300

    Rename build/ to dist/

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: './index.js',
 
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     filename: 'bundle.js',
   },
