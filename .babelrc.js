1a312517c7492e8c0ff384a6a459c7b21f7be8c3
Christian Kaisermann <christian@kaisermann.me>
2018-04-05 16:30:12 -0300

    Update to babel 7. Polyfills are automatically defined by necessity.

---
+++ .babelrc.js
@@ -0,0 +1,37 @@
+const ENV = process.env.NODE_ENV || 'development'
+const IS_PROD = ENV === 'production'
+const IS_TEST = ENV === 'test'
+const IS_DEV = ENV === 'development'
+
+const presets = [
+  [
+    '@babel/preset-env',
+    {
+      /** Polyfill only used built-ins */
+      useBuiltIns: 'usage',
+      loose: true,
+      /** Only parse modules if testing. If not, let webpack handle it */
+      modules: IS_TEST ? 'commonjs' : false,
+      debug: false,
+      forceAllTransforms: true,
+    },
+  ],
+  ['@babel/preset-stage-0', { loose: true }],
+  ['@babel/preset-react', { development: IS_DEV, pragma: 'h' }],
+]
+
+const plugins = ['@babel/plugin-proposal-decorators']
+
+if (IS_PROD) {
+  /** Remove component's proptypes and 'prop-types' package import */
+  plugins.push([
+    'transform-react-remove-prop-types',
+    { mode: 'remove', removeImport: true },
+  ])
+}
+
+module.exports = {
+  sourceMaps: true,
+  presets,
+  plugins,
+}
