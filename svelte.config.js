c9803508bfd60450a461599b2f9d5ccc807f305e
Christian Kaisermann <christian@kaisermann.me>
2018-05-16 18:48:59 -0300

    Add svelte dev and legacy mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
 const getPreprocessor = require('svelte-preprocess')
+const { IS_DEV } = require('quickenv')
 
 module.exports = {
+  dev: IS_DEV,
+  legacy: true,
   preprocess: getPreprocessor(),
 }
