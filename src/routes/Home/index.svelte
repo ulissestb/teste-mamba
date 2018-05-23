c614add6dc10080967cc48d7e9e4f22f3ef64cbc
Christian Kaisermann <christian@kaisermann.me>
2018-05-23 17:10:13 -0300

    Rename CollectionRow to Row

--- src/routes/Home/index.svelte
+++ src/routes/Home/index.svelte
@@ -1,50 +1,50 @@
 <Collection title="Rede">
-  <CollectionRow label="1. Configurar" href="/network/configure"/>
+  <Row label="1. Configurar" href="/network/configure"/>
 </Collection>
 
 <Collection title="Dispositivo">
-  <CollectionRow label="2. Som do Teclado">
+  <Row label="2. Som do Teclado">
     <div slot="controller">
       <Switch bind:checked="isKeyboardSoundEnabled" on:change="toggleKeyboardSound()"/>
     </div>
-  </CollectionRow>
-  <CollectionRow label="3. Luz do Teclado">
+  </Row>
+  <Row label="3. Luz do Teclado">
     <div slot="controller">
       <Switch bind:checked="isKeyboardLightEnabled" on:change="toggleKeyboardLight()"/>
     </div>
-  </CollectionRow>
-  <CollectionRow label="4. Energia" href="/device/energy"/>
+  </Row>
+  <Row label="4. Energia" href="/device/energy"/>
 </Collection>
 
 <Collection title="Senha">
-  <CollectionRow label="5. Alterar senha" href="/password/change"/>
-  <CollectionRow label="6. Esqueci minha senha" href="/password/forgot"/>
+  <Row label="5. Alterar senha" href="/password/change"/>
+  <Row label="6. Esqueci minha senha" href="/password/forgot"/>
 </Collection>
 
 <Collection title="Lojista">
-  <CollectionRow label="7. Recibos" href="/store/receipts"/>
-  <CollectionRow label="8. ID do Pedido">
+  <Row label="7. Recibos" href="/store/receipts"/>
+  <Row label="8. ID do Pedido">
     <div slot="controller">
       <Switch checked disabled />
     </div>
-  </CollectionRow>
-  <CollectionRow label="9. Parcelado Emissor">
+  </Row>
+  <Row label="9. Parcelado Emissor">
     <div slot="controller">
       <Switch checked />
     </div>
-  </CollectionRow>
-  <CollectionRow label="10. Restaurar dados" href="/store/restore"/>
+  </Row>
+  <Row label="10. Restaurar dados" href="/store/restore"/>
 </Collection>
 
 <script>
   import System from '@mamba/native/system'
-  import { Collection, CollectionRow } from '@mamba/collection'
+  import { Collection, Row } from '@mamba/collection'
 
   export default {
     components: {
       Switch: '@mamba/switch',
       Collection,
-      CollectionRow,
+      Row,
     },
     data() {
       return {
--- src/routes/Network/Configure.svelte
+++ src/routes/Network/Configure.svelte
@@ -1,26 +1,26 @@
 <Collection title="Lojista">
-  <CollectionRow label="7. Recibos" href="/store/receipts"/>
-  <CollectionRow label="8. ID do Pedido">
+  <Row label="7. Recibos" href="/store/receipts"/>
+  <Row label="8. ID do Pedido">
     <div slot="controller">
       <Switch checked disabled />
     </div>
-  </CollectionRow>
-  <CollectionRow label="9. Parcelado Emissor">
+  </Row>
+  <Row label="9. Parcelado Emissor">
     <div slot="controller">
       <Switch checked />
     </div>
-  </CollectionRow>
-  <CollectionRow label="10. Restaurar dados" href="/store/restore"/>
+  </Row>
+  <Row label="10. Restaurar dados" href="/store/restore"/>
 </Collection>
 
 <script>
-  import { Collection, CollectionRow } from '@mamba/collection'
+  import { Collection, Row } from '@mamba/collection'
 
   export default {
     components: {
       Switch: '@mamba/switch',
       Collection,
-      CollectionRow,
+      Row,
     },
   }
 
