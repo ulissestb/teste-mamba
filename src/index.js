b4f04333e4cf6d08e713a26a2e6f0c1e62c50d14
Christian Kaisermann <christian@kaisermann.me>
2018-05-23 17:18:01 -0300

    Prevent build:dev from not working on the POS

--- src/index.js
+++ src/index.js
@@ -6,19 +6,23 @@ createHashHistory()
 const root = document.getElementById('root')
 const bootstrapAppFrom = target => new App({ target })
 
-/** If developing, wrap the app with a <POS></POS> */
 if (process.env.NODE_ENV === 'production') {
   window.app = bootstrapAppFrom(root)
 } else {
-  import('@mamba/pos').then(({ default: POS }) => {
-    const appContainer = document.createElement('DIV')
-    bootstrapAppFrom(appContainer)
+  /** If developing, wrap the app with a <POS></POS> if running on a web server */
+  if (window.location.protocol === 'file:') {
+    window.app = bootstrapAppFrom(root)
+  } else {
+    import('@mamba/pos').then(({ default: POS }) => {
+      const appContainer = document.createElement('DIV')
+      bootstrapAppFrom(appContainer)
 
-    window.app = new POS({
-      target: root,
-      slots: {
-        default: appContainer,
-      },
+      window.app = new POS({
+        target: root,
+        slots: {
+          default: appContainer,
+        },
+      })
     })
-  })
+  }
 }
