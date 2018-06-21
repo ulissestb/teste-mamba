8dbc3557073063ea2e3831096cf575ae146ca1e3
Christian Kaisermann <christian@kaisermann.me>
2018-06-21 19:04:52 -0300

    Make svelte use postcss

--- svelte.config.js
+++ svelte.config.js
@@ -4,12 +4,16 @@ const { IS_DEV } = require('quickenv')
 module.exports = {
   dev: IS_DEV(),
   legacy: true,
-  preprocess: getPreprocessor(),
-  onwarn(warning, handler) {
-    /** Prevent warning on autofocus attributes */
-    if (warning.code === 'a11y-autofocus') {
-      return
-    }
-    handler(warning)
-  },
+  preprocess: getPreprocessor({
+    transformers: {
+      postcss: true,
+    },
+  }),
+  // onwarn(warning, handler) {
+  //   /** Prevent warning on autofocus attributes */
+  //   if (warning.code === 'a11y-autofocus') {
+  //     return
+  //   }
+  //   handler(warning)
+  // },
 }
