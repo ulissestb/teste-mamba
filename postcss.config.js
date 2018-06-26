83f61785ed23c8b0511eb8b541374295d978dbd2
Christian Kaisermann <christian@kaisermann.me>
2018-06-26 18:55:58 -0300

    Add stylelint to postcss build step

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,20 @@
+const stylelint = require('stylelint')
+
 module.exports = {
   plugins: [
-    require('postcss-easy-import'),
-    require('postcss-advanced-variables'),
-    require('postcss-atroot'),
+    stylelint,
+    require('postcss-easy-import')({
+      extensions: ['.css', '.pcss'],
+      plugins: [stylelint],
+    }),
     require('postcss-extend-rule'),
-    require('postcss-nested'),
+    require('postcss-advanced-variables'),
     require('postcss-preset-env')({
       stage: 2 /** Defaults postcss-preset-env to stage 2 */,
     }),
+    require('postcss-atroot'),
     require('postcss-property-lookup'),
+    require('postcss-nested'),
+    require('postcss-reporter')({ clearReportedMessages: true }),
   ],
 }
