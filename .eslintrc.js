4ae73021f3b76fdbb13d17bb37d90d58be918263
Christian Kaisermann <christian@kaisermann.me>
2018-07-18 16:43:43 -0300

    Add @mamba/configs package and dummy saved packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,8 @@
-const { IS_PROD } = require('quickenv')
-
 module.exports = {
+  // TODO: Change path when packages are published
   extends: [
-    'standard',
-    'prettier',
-    'prettier/standard',
-    'plugin:jest/recommended',
-    'plugin:import/recommended',
+    './node_modules/@mamba/configs/eslint/index.js',
+    './node_modules/@mamba/configs/eslint/jest.js',
+    './node_modules/@mamba/configs/eslint/template.js',
   ],
-  plugins: ['standard', 'prettier', 'html', 'import', 'jest'],
-  settings: {
-    'html/html-extensions': ['.html', '.svelte'],
-  },
-  env: {
-    browser: true,
-    node: true,
-    es6: true,
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    /** Enforce indentation of two spaces */
-    indent: ['error', 2, { SwitchCase: 1 }],
-
-    /** Enforce trailing comma */
-    'comma-dangle': ['error', 'always-multiline'],
-
-    /** Allow __meta__ variable */
-    'no-underscore-dangle': 'off',
-
-    /** Disallow 'console.log' on production */
-    'no-console': IS_PROD()
-      ? [
-          'error',
-          {
-            allow: ['warn', 'error'],
-          },
-        ]
-      : 'off',
-
-    /** Disallow 'var' */
-    'no-var': 'error',
-
-    /** Allow unstored instantiations */
-    'no-new': 'off',
-
-    /** Enforce file extensions on 'import' statements */
-    'import/extensions': ['error', 'ignorePackages'],
-
-    /** Don't complain about non-module svelte files */
-    'import/no-unresolved': [
-      'error',
-      {
-        ignore: ['.(?:svelte|html)$', '^(@mamba[\\/]|svelte-)'],
-      },
-    ],
-  },
-  overrides: [
-    {
-      files: ['**/__tests__/**/*.js', '**/*.test.js'],
-      env: { jest: true },
-    },
-  ],
-}
+};
