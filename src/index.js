3b29cf12bc83832e4b74f8ec2078ef62030b5110
Christian Kaisermann <christian@kaisermann.me>
2018-08-08 17:26:11 -0300

    Add simulator runtime

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
+/**
+ * Boot the actual app into the `app-root` element.
+ * * Modify this file only if you know what you are doing.
+ */
 import App from './App.html';
 import store from './store.js';
 
-/**
- * Bootstrap the application.
- * If developing, it wraps the whole app within a '@mambasdk/pos' for better visuals.
- * If the code is running on the production environment, just initialize the app.
- *
- * We use dynamic import here for preventing webpack from bundling the @mambasdk/pos
- * even in a production environment build.
- * */
-const root = document.getElementById('root');
-
-if (__POS__) {
-  new App({ target: root, store });
-} else if (__BROWSER__) {
-  if (__DEV__) {
-    window.MambaStore = store;
-  }
-  /** If developing, wrap the app with a <POS></POS> */
-  import('@mambasdk/pos/simulator/view/index.js').then(({ default: POS }) => {
-    const appFragment = document.createDocumentFragment();
-    new App({ target: appFragment, store });
-    new POS({
-      target: root,
-      store,
-      slots: {
-        default: appFragment,
-      },
-    });
-  });
-}
+new App({
+  target: document.getElementById('app-root'),
+  store,
+});
---
+++ src/simulator.js
@@ -0,0 +1,11 @@
+/**
+ * Boot the Mamba Simulator before the app is initialized.
+ * * Usually, there's no need to modify this file.
+ */
+import * as Simulator from '@mambasdk/pos/simulator/index.js';
+import store from './store.js';
+
+/** Initialize the Simulator View */
+new Simulator.POS({ target: document.body, store });
+
+export default Simulator;
