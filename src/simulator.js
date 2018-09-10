b84eaa94adff072d660319f6ac2f54a880afad2b
Christian Kaisermann <christian@kaisermann.me>
2018-09-10 11:08:42 -0300

    Simplify simulator runtime file

--- src/simulator.js
+++ src/simulator.js
@@ -2,11 +2,9 @@
  * Boot the Mamba Simulator before the app is initialized.
  * * Usually, there's no need to modify this file.
  */
-import * as Simulator from '@mamba/pos/simulator/index.js';
+import '@mamba/pos/simulator/index.js';
 import POS from '@mamba/pos/simulator/view.js';
 import store from './store.js';
 
 /** Initialize the Simulator View */
 new POS({ target: document.body, store });
-
-export default Simulator;
