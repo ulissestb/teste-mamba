cb05d6dfd79392264aac2fa98c351a6aff3f8bbc
Christian Kaisermann <christian@kaisermann.me>
2018-09-28 18:31:12 -0300

    Remove unnecessary store

--- src/simulator.js
+++ src/simulator.js
@@ -3,7 +3,6 @@
  * * Usually, there's no need to modify this file.
  */
 import * as Simulator from '@mamba/pos/simulator/index.js';
-import store from './store.js';
 
 /** Initialize the Simulator View */
-Simulator.getVirtualPOS(store);
+Simulator.getVirtualPOS();
